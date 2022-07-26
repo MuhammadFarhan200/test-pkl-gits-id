757952e674f0f37dc354521882dfc38450443c8e
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-07-26 15:45:43 +0700

    tes pkl gits

--- test1.php
+++ test1.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test PKL Gits | Soal 1</title>
-</head>
-<body>
-    
-    <?php  ?>
+<?php
+function i($arr)
+{
+    $c = 0;
+    $r = 0;
 
-</body>
-</html>
+    foreach ($arr as $item) {
+        if ($item == 1) {
+            return $c += 1;
+        }
+        $r = $arr < $c && ($arr = $c);
+        if ($c == 0 && $item == 0) {
+            return $c = 0;
+        }
+    }
+}
+
+$arr = [1, 0, 0, 1, 0, 1, 1];
+echo i($arr);
+
+?>
