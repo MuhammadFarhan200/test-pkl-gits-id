a9998193f0241c4047bdf89a351ccc1028ac71ed
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-07-27 08:51:20 +0700

    tes pkl gits

--- test1.php
+++ test1.php
@@ -1,21 +1,9 @@
 <?php
-function i($arr)
-{
-    $c = 0;
-    $r = 0;
 
-    foreach ($arr as $item) {
-        if ($item == 1) {
-            return $c += 1;
-        }
-        $r = $arr < $c && ($arr = $c);
-        if ($c == 0 && $item == 0) {
-            return $c = 0;
-        }
-    }
+$arr = [1, 1, 0, 1, 1, 1];
+
+for ($i = 5; $i < count($arr); $i++) {
+    echo $arr[$i] += 2;
 }
-
-$arr = [1, 0, 0, 1, 0, 1, 1];
-echo i($arr);
 
 ?>
